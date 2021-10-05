a9ded80b0c05bfc3d7eaa202500e3d092b5bc54f
Psuedoo <forthewin720@gmail.com>
2021-10-05 11:48:33 -0400

    added container_name; addding addition function

--- cogs/basic.py
+++ cogs/basic.py
@@ -11,6 +11,17 @@ class Basic(commands.Cog):
     async def is_up(self, ctx):
         await ctx.send('I am alive!')
 
+    @commands.command(name="add")
+    async def add(self, ctx, num_one, num_two):
+        if num_one and num_two:
+            try:
+                num_one = int(num_one)
+                num_two = int(num_two)
+                await ctx.send(num_one+num_two)
+            except TypeError as e:
+                print(e)
+        else:
+            print("Something's wrong")
 
 def setup(bot):
     bot.add_cog(Basic(bot))
